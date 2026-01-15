537db2e32c271338f89ac02a0aa4de098545192f
ChrisDerFDSler <c.froehlich@fds-limburg.schule>
2026-01-15 08:26:20 +0100

    Update E-Mailversenden.py

--- Code/E-Mailversenden.py
+++ Code/E-Mailversenden.py
@@ -5,17 +5,18 @@ from email.message import EmailMessage
 
 msg = EmailMessage()
 msg['Subject'] = 'I NEED FOOOD'
-msg['From'] = 'c.froehlich@fds-limburg.schule'
-msg['To'] = 'nikita.puidenko@fds-limburg.schule'
+msg['From'] = 'deine.email'
+msg['To'] = 'Ziel E-mail'
 msg.set_content('ICH HABE HUNGER, GIB MIR ESSEN')
 
 
 
 
-with smtplib.SMTP_SSL('smtp.fds-limburg.schule') as smtp:
-    smtp.login('c.froehlich', 'dc.rWj#=^Dz4!;#')
+with smtplib.SMTP_SSL('smtp.domain') as smtp:
+    smtp.login('Name', 'passwort')
     smtp.send_message(msg)
 
 
 
 print('E-Mail erfolgreich versendet!')
+
